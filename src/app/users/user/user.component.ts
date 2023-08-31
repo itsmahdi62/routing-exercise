adba7ea410dcd0c752210db26b1b334ebb5e1bfd
Mahdi <amiralmasi021@gmail.com>
2023-08-31 15:59:32 +0330

    Add onDestroy to remove useless observales

--- src/app/users/user/user.component.ts
+++ src/app/users/user/user.component.ts
@@ -1,14 +1,15 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 import { ActivatedRoute, Params } from '@angular/router';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-user',
   templateUrl: './user.component.html',
   styleUrls: ['./user.component.css']
 })
-export class UserComponent implements OnInit {
+export class UserComponent implements OnInit ,OnDestroy {
   user: {id: number, name: string} | any ;
-
+  paramsSubscription:Subscription | any;
 
   constructor(private route:ActivatedRoute) { }
 
@@ -17,11 +18,16 @@ export class UserComponent implements OnInit {
       id:this.route.snapshot.params['id'],
       name:this.route.snapshot.params['name'],
     };
-      this.route.params.subscribe(
+
+    this.paramsSubscription = this.route.params.subscribe(
         (params:Params) =>{
           this.user.id = params['id'];
           this.user.id = params['name'];
         }
       );
+  }
+
+  ngOnDestroy(): void {
+    this.paramsSubscription.unsubscrive();
   }
 }
